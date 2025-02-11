e0f1a55badf9040d642afc607f63f6d6e114516d
Jean-Philippe Binger <jean-philippe.binger@epita.fr>
2025-02-11 11:35:23 +0100

    Added default for userscontract, need usercontract address to create Tweet contract, fix checking user exist in send tweet

--- contracts.py
+++ contracts.py
@@ -32,20 +32,27 @@ def main():
             assert self.data.users.contains(sp.sender), "NO_USER_TO_DELETE"
             self.data.users[sp.sender].deleted = True
 
+        @sp.entrypoint
+        def default(self,user_address):
+            assert self.data.users.contains(user_address) and not self.data.users[user_address].deleted, "USER_DOES_NOT_EXIST"
+
     # TWEETS
     class TweetContract(sp.Contract):
-        def __init__(self):
+        def __init__(self,users_address):
             self.data.next_id=sp.nat(0)
             self.data.tweets=sp.big_map({})
+            self.data.users_address = users_address
             sp.cast(self.data.tweets, sp.big_map[sp.nat, sp.record(
                     author=sp.address,
                     content=sp.string,
                     timestamp=sp.timestamp,
                     deleted=sp.bool)])
-    
+
         @sp.entry_point
         def post_tweet(self, content):
             assert len(content) <= 280, "ERROR_TWEET_TOO_LONG"
+            contract = sp.contract(sp.address,self.data.users_address,entrypoint="default").unwrap_some()
+            sp.transfer(sp.sender,sp.mutez(0),contract)
             self.data.tweets[self.data.next_id] = sp.record(
                 author = sp.sender,
                 content = content,
@@ -60,24 +67,26 @@ def main():
             assert self.data.tweets[id].author == sp.sender, "NOT_RIGHT_PERSON"
             self.data.tweets[id].deleted = True
 
+
+
 @sp.add_test()
 def test():
     user1 = sp.address("tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")
     user2 = sp.address("tz1aSkwEot3L2kmUvcoxzjMomb9mvBNuzFK6")
     user3 = sp.address("tz1afjrfnivoisnvdisubvospdncvr945dzd")
-    
+
     scenario = sp.test_scenario("User Smart Contract", main)
     c1 = main.UserContract()
     scenario += c1
-    
+
     c1.create_user(username = "Pablito", bio = "Hello, Glenn!", _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 7, 12, 0, 0))
     scenario.verify(c1.data.users[user1].username == "Pablito")
     scenario.verify(c1.data.users[user1].bio == "Hello, Glenn!")
-    
+
     c1.create_user(username = "CacaProut", bio = "Hello again, Glenn", _sender=user2, _now=sp.timestamp_from_utc(2025, 2, 7, 12, 5, 0))
     scenario.verify(c1.data.users[user2].username == "CacaProut")
     scenario.verify(c1.data.users[user2].bio == "Hello again, Glenn")
-    
+
     long_bio = "A" * 151
     c1.create_user(username = "MegaCaca", bio = long_bio, _sender=user1, _valid=False, _exception="BIO_TOO_LONG")
 
@@ -92,25 +101,27 @@ def test():
     c1.delete_user(_sender=user3, _valid=False, _exception="NO_USER_TO_DELETE")
 
 
-    c2 = main.TweetContract()
+    c2 = main.TweetContract(c1.address)
     scenario += c2
-    
+
     c2.post_tweet("Hello, world!", _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 7, 12, 10, 0))
     scenario.verify(c2.data.tweets[0].author == user1)
     scenario.verify(c2.data.tweets[0].content == "Hello, world!")
     scenario.verify(c2.data.tweets[0].deleted == False)
-    
+
     c2.post_tweet("Another day, another tweet.", _sender=user2, _now=sp.timestamp_from_utc(2025, 2, 7, 12, 15, 0))
     scenario.verify(c2.data.tweets[1].author == user2)
     scenario.verify(c2.data.tweets[1].content == "Another day, another tweet.")
-    
+
     long_tweet = "A" * 281
     c2.post_tweet(long_tweet, _sender=user1, _valid=False, _exception="ERROR_TWEET_TOO_LONG")
-    
+
+    c2.post_tweet("AAAA", _sender=user3, _valid=False, _exception="USER_DOES_NOT_EXIST")
+
     c2.delete_tweet(0, _sender=user1)
     scenario.verify(c2.data.tweets[0].deleted == True)
-        
+
     c2.delete_tweet(1, _sender=user1, _valid=False, _exception="NOT_RIGHT_PERSON")
-    
+
     c2.delete_tweet(1, _sender=user2)
     scenario.verify(c2.data.tweets[1].deleted == True)
