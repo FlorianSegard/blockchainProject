71853942568746635d4d9aa325124155627ee406
Jean-Philippe Binger <jean-philippe.binger@epita.fr>
2025-02-15 16:26:37 +0100

    fix limitation and double delete tweet

--- contracts.py
+++ contracts.py
@@ -14,6 +14,8 @@ def main():
                 username=sp.string,
                 bio=sp.string,
                 timestamp=sp.timestamp,
+                last_username_change=sp.timestamp,
+                last_bio_change=sp.timestamp,
                 deleted=sp.bool
             )])
             sp.cast(self.data.deposits, sp.big_map[sp.address, sp.mutez])
@@ -24,12 +26,14 @@ def main():
             assert len(bio) <= 150, "BIO_TOO_LONG"
             assert not self.data.users.contains(sp.sender) or self.data.users[sp.sender].deleted, "ALREADY_CREATED_USER"
             assert sp.amount >= self.data.DEPOSIT_AMOUNT, "INSUFFICIENT_DEPOSIT"
-            
+
             self.data.users[sp.sender] = sp.record(
                 id=self.data.next_id,
                 username=username,
                 bio=bio,
                 timestamp=sp.now,
+                last_username_change=sp.now,
+                last_bio_change=sp.now,
                 deleted=False
             )
             self.data.deposits[sp.sender] = sp.amount
@@ -38,29 +42,37 @@ def main():
         @sp.entry_point
         def change_username(self, new_username):
             assert self.data.users.contains(sp.sender), "UNKNOWN_USER"
+            assert not self.data.users[sp.sender].deleted, "DELETED_USER"
+            assert sp.now - self.data.users[sp.sender].last_username_change > 24*60*60, "USERNAME_CHANGE_TOO_FREQUENT"
             assert len(new_username) <= 15, "USERNAME_TOO_LONG"
-            
+
             self.data.users[sp.sender].username = new_username
 
         @sp.entry_point
         def change_bio(self, new_bio):
             assert self.data.users.contains(sp.sender), "UNKNOWN_USER"
+            assert not self.data.users[sp.sender].deleted, "DELETED_USER"
+            assert sp.now - self.data.users[sp.sender].last_bio_change > 4*60*60, "BIO_CHANGE_TOO_FREQUENT"
             assert len(new_bio) <= 150, "BIO_TOO_LONG"
-            
+
             self.data.users[sp.sender].bio = new_bio
 
         @sp.entry_point
         def delete_user(self):
             assert self.data.users.contains(sp.sender), "NO_USER_TO_DELETE"
             assert not self.data.users[sp.sender].deleted, "USER_ALREADY_DELETED"
-            
+
             self.data.users[sp.sender].deleted = True
             sp.send(sp.sender, self.data.deposits[sp.sender])
             self.data.deposits[sp.sender] = sp.tez(0)
 
         @sp.entrypoint
-        def default(self, user_address):
+        def verified(self, user_address):
             assert self.data.users.contains(user_address) and not self.data.users[user_address].deleted, "USER_DOES_NOT_EXIST"
+
+        @sp.entrypoint
+        def default(self):
+            pass
 
     # TWEETS
     class TweetContract(sp.Contract):
@@ -68,6 +80,7 @@ def main():
             self.data.next_id = sp.nat(0)
             self.data.tweets = sp.big_map({})
             self.data.users_address = users_address
+            self.data.last_tweet_time = sp.big_map({})
             sp.cast(self.data.tweets, sp.big_map[sp.nat, sp.record(
                 author=sp.address,
                 content=sp.string,
@@ -78,7 +91,9 @@ def main():
         @sp.entry_point
         def post_tweet(self, content):
             assert len(content) <= 280, "ERROR_TWEET_TOO_LONG"
-            contract = sp.contract(sp.address, self.data.users_address, entrypoint="default").unwrap_some()
+            if self.data.last_tweet_time.contains(sp.sender):
+                assert sp.now - self.data.last_tweet_time[sp.sender] > 60, "TOO_MANY_TWEETS_TOO_FAST"
+            contract = sp.contract(sp.address, self.data.users_address, entrypoint="verified").unwrap_some()
             sp.transfer(sp.sender, sp.mutez(0), contract)
             self.data.tweets[self.data.next_id] = sp.record(
                 author=sp.sender,
@@ -86,11 +101,12 @@ def main():
                 timestamp=sp.now,
                 deleted=False
             )
+            self.data.last_tweet_time[sp.sender] = sp.now
             self.data.next_id += 1
 
         @sp.entry_point
         def delete_tweet(self, id):
-            assert self.data.tweets.contains(id), "NO_TWEET_TO_DELETE"
+            assert self.data.tweets.contains(id) and not self.data.tweets[id].deleted, "NO_TWEET_TO_DELETE"
             assert self.data.tweets[id].author == sp.sender, "NOT_RIGHT_PERSON"
             self.data.tweets[id].deleted = True
 
@@ -105,16 +121,16 @@ def test():
     scenario += c1
 
     c1.create_user(username="Pablito", bio="Hello, Glenn!", _sender=user1, _amount=sp.tez(20), _now=sp.timestamp_from_utc(2025, 2, 7, 12, 0, 0), _valid=False, _exception="INSUFFICIENT_DEPOSIT")
-    
+
     c1.create_user(username="Pablito", bio="Hello, Glenn!", _sender=user1, _amount=sp.tez(50), _now=sp.timestamp_from_utc(2025, 2, 7, 12, 0, 0))
     scenario.verify(c1.data.users[user1].username == "Pablito")
     scenario.verify(c1.data.users[user1].bio == "Hello, Glenn!")
 
-    c1.create_user(username="CacaProut", bio="Hello again, Glenn", _sender=user2, _amount=sp.tez(50), _now=sp.timestamp_from_utc(2025, 2, 7, 12, 0, 0))
-    scenario.verify(c1.data.users[user2].username == "CacaProut")
+    c1.create_user(username="Carlito", bio="Hello again, Glenn", _sender=user2, _amount=sp.tez(50), _now=sp.timestamp_from_utc(2025, 2, 7, 12, 0, 0))
+    scenario.verify(c1.data.users[user2].username == "Carlito")
 
     long_bio = "A" * 151
-    c1.create_user(username="MegaCaca", bio=long_bio, _sender=user1, _amount=sp.tez(50), _valid=False, _exception="BIO_TOO_LONG")
+    c1.create_user(username="Marlito", bio=long_bio, _sender=user1, _amount=sp.tez(50), _valid=False, _exception="BIO_TOO_LONG")
 
     c1.delete_user(_sender=user1)
     scenario.verify(c1.data.users[user1].deleted == True)
@@ -125,18 +141,21 @@ def test():
     long_username = "A" * 16
     c1.create_user(username=long_username, bio="aaaaa", _sender=user3, _amount=sp.tez(50), _now=sp.timestamp_from_utc(2025, 2, 7, 12, 0, 0), _valid=False, _exception="USERNAME_TOO_LONG")
 
-    c1.change_username(long_username, _sender=user1, _valid=False, _exception="USERNAME_TOO_LONG")
-    c1.change_bio(long_bio, _sender=user1, _valid=False, _exception="BIO_TOO_LONG")
+    c1.change_username(long_username, _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 8, 12, 0, 1), _valid=False, _exception="USERNAME_TOO_LONG")
+    c1.change_bio(long_bio, _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 8, 16, 0, 1), _valid=False, _exception="BIO_TOO_LONG")
 
-    c1.change_username(long_username, _sender=user3, _valid=False, _exception="UNKNOWN_USER")
-    c1.change_bio(long_bio, _sender=user3, _valid=False, _exception="UNKNOWN_USER")
+    c1.change_username(long_username, _now=sp.timestamp_from_utc(2025, 2, 8, 12, 0, 1), _sender=user3, _valid=False, _exception="UNKNOWN_USER")
+    c1.change_bio(long_bio, _sender=user3, _now=sp.timestamp_from_utc(2025, 2, 7, 16, 0, 1), _valid=False, _exception="UNKNOWN_USER")
 
-    c1.change_username("MessoGlenn", _sender=user1)
-    c1.change_bio("MessoGlennButItsTheBio", _sender=user1)
+    c1.change_username("MessoGlenn", _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 8, 12, 0, 0), _valid=False, _exception="USERNAME_CHANGE_TOO_FREQUENT")
+    c1.change_bio("MessoGlennButItsTheBio", _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 7, 16, 0, 0), _valid=False, _exception="BIO_CHANGE_TOO_FREQUENT")
+
+    c1.change_username("MessoGlenn", _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 8, 12, 0, 1))
+    c1.change_bio("MessoGlennButItsTheBio", _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 7, 16, 0, 1))
     scenario.verify(c1.data.users[user1].username == "MessoGlenn")
     scenario.verify(c1.data.users[user1].bio == "MessoGlennButItsTheBio")
 
-    
+
     c2 = main.TweetContract(c1.address)
     scenario += c2
     c2.post_tweet("Hello, world!", _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 7, 12, 0, 0))
@@ -144,6 +163,26 @@ def test():
     scenario.verify(c2.data.tweets[0].content == "Hello, world!")
     scenario.verify(c2.data.tweets[0].deleted == False)
 
+    c2.post_tweet("Hello!", _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 7, 12, 0, 0), _valid=False, _exception="TOO_MANY_TWEETS_TOO_FAST")
+    c2.post_tweet("world", _sender=user1, _now=sp.timestamp_from_utc(2025, 2, 7, 12, 1, 1))
+
+
+    c2.post_tweet("Another day, another tweet.", _sender=user2, _now=sp.timestamp_from_utc(2025, 2, 7, 12, 15, 0))
+    scenario.verify(c2.data.tweets[2].author == user2)
+    scenario.verify(c2.data.tweets[2].content == "Another day, another tweet.")
+
+    long_tweet = "A" * 281
+    c2.post_tweet(long_tweet, _sender=user1, _valid=False, _exception="ERROR_TWEET_TOO_LONG")
+
+    c2.post_tweet("AAAA", _sender=user3, _valid=False, _exception="USER_DOES_NOT_EXIST")
+
+
     c2.delete_tweet(0, _sender=user1)
     scenario.verify(c2.data.tweets[0].deleted == True)
-    c2.delete_tweet(1, _sender=user2, _valid=False, _exception="NO_TWEET_TO_DELETE")
+    c2.delete_tweet(0, _sender=user1, _valid=False, _exception="NO_TWEET_TO_DELETE")
+    c2.delete_tweet(0, _sender=user2, _valid=False, _exception="NO_TWEET_TO_DELETE")
+
+    c2.delete_tweet(2, _sender=user1, _valid=False, _exception="NOT_RIGHT_PERSON")
+
+    c2.delete_tweet(2, _sender=user2)
+    scenario.verify(c2.data.tweets[2].deleted == True)
